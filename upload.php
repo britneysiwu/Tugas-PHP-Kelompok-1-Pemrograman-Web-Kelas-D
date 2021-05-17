271b6eb6a741fe81aae387be4a7a91b865ce30ca
britneysiwu <83330602+britneysiwu@users.noreply.github.com>
2021-05-17 20:40:32 +0800

    final2 page upload

--- upload.php
+++ upload.php
@@ -1,39 +1,45 @@
 <?php
-
-function upload() {
-
-$file = $_FILES['berkas'];
-$nama_file = $file['name'];
-$nama_tmp = $file['tmp_name'];
-$upload_dir = "upload/";
-$error = $file['error'];
-
-
-if( $error === 4 ) {
-    echo "<script>
-            alert('Pilih file terlebih dahulu!');
-        </script>";
-        return false;
-}
-
-$ekstensiBerkasValid = ['jpg', 'jpeg', 'png', 'doc', 'pdf'];
-$ekstensiBerkas = explode('.', $nama_file);
-$ekstensiBerkas = strtolower(end($ekstensiBerkas));
-if( !in_array($ekstensiBerkas, $ekstensiBerkasValid)) {
-    echo "<script>
-            alert('Maaf, file yang didukung hanya: jpg, jpeg, png, doc, pdf');
-        </script>";
-        return false;
-} else {
-    echo "<script>
-            alert('File berhasil diupload');
-        </script>";
-}
-
-move_uploaded_file($nama_tmp,$upload_dir.$nama_file);
-
-}
-
-upload();
-
+session_start();
+    //menghalangi user pindah ke halaman upload apabila user belum berhasil login
+    if( !isset($_SESSION["login"]) ) {
+        header("Location: login.php");
+        exit;
+    }
+    
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="upstyle.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+    
+    <h1>Silahkan Unggah File</h1>
+
+    <form action="upload2.php" method="post" enctype="multipart/form-data">
+    
+    
+    <input type="file" name="berkas" id="berkas">
+    
+
+    <br>
+    <br>
+    
+    <input type="submit" name="upload" id="upload" value="Upload" />
+
+
+    <br>
+    <a href="home.php">Kembali</a>
+    </div>
+
+</form>
+    
+</body>
+</html>
