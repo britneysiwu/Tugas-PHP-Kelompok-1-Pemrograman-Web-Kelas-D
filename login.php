e056e561a338017122e3cab5a8c8899d7543db77
britneysiwu <83330602+britneysiwu@users.noreply.github.com>
2021-05-17 12:23:08 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-//ketika sudah login, user tetap berada di halaman upload
+//ketika sudah login, user tetap berada di halaman
 if( isset($_SESSION["login"]) ) {
     header("Location: upload.php");
     exit;
@@ -12,7 +12,7 @@ if( isset($_POST["submit"]) ) {
     if( $_POST["username"] == "britney" && $_POST["password"] == "3112") {
     //jika benar, redirect ke halaman admin
     $_SESSION["login"] = true;
-    header("Location: upload.html");
+    header("Location: upload.php");
     exit;
     } else {
     //jika salah, tampilkan pesan kesalahan
@@ -24,32 +24,45 @@ if( isset($_POST["submit"]) ) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<link rel="stylesheet" type="text/css" href="loginstyle.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <h2>Login</h2>
+
+<div class = container>
+    <h2>LOGIN</h2>
     
     <?php if( isset($error) ) : ?>
-    <p>username atau password salah</p>
+    <p>Username atau Password Salah!</p>
     <?php endif; ?>
     
-    <ul>
+    
     <form action="" method="post">
-        <li>
-            <label for="username">Username:</label>
+        <div class="inform">
+        <div class="row">
+            
+            <label for="username">Username </label>            
             <input type="text" name="username" id="username">
-        </li>
-        <li>
-            <label for="password">Password:</label>
+            </div>
+            </div>
+        
+            
+            <label for="password">Password </label>
             <input type="password" name="password" id="password">
-        </li>
-        <li>
+            </div>
+        
+            <div class="buttons">
             <button type="submit" name="submit">Login</button>
-        </li>
+    </div>
+        </div>
+        
     </form>
-    </ul>
+    
+    </div>
+
+
 </body>
 </html>
